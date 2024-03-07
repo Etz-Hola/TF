705673f7fc9f2c55f07ae8e649496a6f104777d6
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2024-03-07 21:22:58 +0100

    uu

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,19 +1,50 @@
-import { Box, Flex, Image, Link } from "@chakra-ui/react";
-import React from "react";
+import { Box, Flex, Text } from "@chakra-ui/react";
+
 
 const Header = () => {
   return (
     <>
-      <header className='header'>
-        <Box >        
-        <Image bg={"#fff"} src="/herrobag.png" borderRadius={8}  />
-        </Box>
 
-        <Box w='100%' h='200px' bgGradient='linear(to-r, green.200, pink.500)' />
+      <Flex
+        px={{ base: 8, md: 20 }}
+        justifyContent={{ md: "space-between" }} 
+        alignItems={"center"}
+        gap={{ md: 10, lg: 5 }}
+        w={"full"}
+        bgImage={"/herrobag.png"}
+        backgroundRepeat={"no-repeat"}
+        backgroundSize={"cover"}
+        height={"400px"}
+      >
+        <Flex
+          flexDir={"column"}
+          // w={500}
+          gap={{ md: .2, lg: 1 }}
+          justifyContent={"center"}
+          alignItems={"center"}
+        >
+          <Text
+            fontSize={{ base: "3xl", md: "3xl", lg: "5xl" }}
+            fontWeight={100}
+            color={"#fff"}
+            margin={"30"}
+          >
+
+            <Text>Explore Around effortlessly by train and bus</Text>
+          </Text>
+
+          <Box bg="white"
+          w={700}
+          height={39}
+          >
+            
+          </Box>
 
 
-      </header>
-        
+        </Flex>
+      </Flex>
+
+      <Box w='100%' h='200px' bgGradient='linear(to-r, green.200, pink.500)' />
 
     </>
   );
