73866b37a03aa5dafe4cc931c5b5c70f3082d56b
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2024-03-25 11:48:36 +0100

    update

--- client/src/Pages/Train/components/DataTable.jsx
+++ client/src/Pages/Train/components/DataTable.jsx
@@ -1,11 +1,7 @@
-// DataTable.js
-import React from 'react';
+import React from "react";
 
-const DataTable = ({ data, onEdit, onSave }) => {
-  
+const DataTable = ({ data, onEdit, onSave, setTrainData }) => {
   return (
-
-
     <div className="overflow-x-auto">
       <table className="table-auto min-w-full divide-y divide-gray-200">
         <thead className="bg-gray-50">
@@ -20,94 +16,41 @@ const DataTable = ({ data, onEdit, onSave }) => {
               Departure Station
             </th>
             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-            ways
+              Ways
             </th>
             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-            Types
+              Types
             </th>
             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-            Arrival Station
+              Arrival Station
             </th>
             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
               Duration
             </th>
             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-            firstclassPrice
+              Firstclass Price
             </th>
             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-            StanderdPrice
+              Standard Price
             </th>
             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-            availableSeats
+              Available Seats
             </th>
             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-            returnTimeFromArrivalStation
+              Return Time From Arrival Station
             </th>
             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-            StanderdPrice
+              Arrival Time Departure Station
             </th>
             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-            returnTimeFromArrivalStation
+              Edit
             </th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-            arrivalTimeDepartureStation
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-            eddit
-            </th>
-            {/* Add more header columns as needed */}
-           
           </tr>
         </thead>
         <tbody className="bg-white divide-y divide-gray-200">
           {data.map((item, index) => (
-            
             <tr key={item._id}>
               <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                {item.trainId}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                {item.nameOrNumber}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                {item.departureStation}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                {item.ways}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                {item.types}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                {item.arrivalStation}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                {item.departureTime}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                {item.arrivalTime}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                {item.duration}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                {item.firstclassPrice}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                {item.StanderdPrice}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                {item.availableSeats}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                {item.returnTimeFromArrivalStation}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                {item.arrivalTimeDepartureStation}
-              </td>
-              {/* Add more cells for other data */}
-
-              <td>
                 {item.editable ? (
                   <input
                     type="text"
@@ -122,7 +65,173 @@ const DataTable = ({ data, onEdit, onSave }) => {
                   item.trainId
                 )}
               </td>
-              {/* Render other table data cells */}
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                {item.editable ? (
+                  <input
+                    type="text"
+                    value={item.nameOrNumber}
+                    onChange={(e) => {
+                      const newData = [...data];
+                      newData[index].nameOrNumber = e.target.value;
+                      setTrainData(newData);
+                    }}
+                  />
+                ) : (
+                  item.nameOrNumber
+                )}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                {item.editable ? (
+                  <input
+                    type="text"
+                    value={item.departureStation}
+                    onChange={(e) => {
+                      const newData = [...data];
+                      newData[index].departureStation = e.target.value;
+                      setTrainData(newData);
+                    }}
+                  />
+                ) : (
+                  item.departureStation
+                )}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                {item.editable ? (
+                  <input
+                    type="text"
+                    value={item.ways}
+                    onChange={(e) => {
+                      const newData = [...data];
+                      newData[index].ways = e.target.value;
+                      setTrainData(newData);
+                    }}
+                  />
+                ) : (
+                  item.ways
+                )}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                {item.editable ? (
+                  <input
+                    type="text"
+                    value={item.types}
+                    onChange={(e) => {
+                      const newData = [...data];
+                      newData[index].types = e.target.value;
+                      setTrainData(newData);
+                    }}
+                  />
+                ) : (
+                  item.types
+                )}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                {item.editable ? (
+                  <input
+                    type="text"
+                    value={item.arrivalStation}
+                    onChange={(e) => {
+                      const newData = [...data];
+                      newData[index].arrivalStation = e.target.value;
+                      setTrainData(newData);
+                    }}
+                  />
+                ) : (
+                  item.arrivalStation
+                )}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                {item.editable ? (
+                  <input
+                    type="text"
+                    value={item.duration}
+                    onChange={(e) => {
+                      const newData = [...data];
+                      newData[index].duration = e.target.value;
+                      setTrainData(newData);
+                    }}
+                  />
+                ) : (
+                  item.duration
+                )}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                {item.editable ? (
+                  <input
+                    type="text"
+                    value={item.firstclassPrice}
+                    onChange={(e) => {
+                      const newData = [...data];
+                      newData[index].firstclassPrice = e.target.value;
+                      setTrainData(newData);
+                    }}
+                  />
+                ) : (
+                  item.firstclassPrice
+                )}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                {item.editable ? (
+                  <input
+                    type="text"
+                    value={item.standardPrice}
+                    onChange={(e) => {
+                      const newData = [...data];
+                      newData[index].standardPrice = e.target.value;
+                      setTrainData(newData);
+                    }}
+                  />
+                ) : (
+                  item.standardPrice
+                )}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                {item.editable ? (
+                  <input
+                    type="text"
+                    value={item.availableSeats}
+                    onChange={(e) => {
+                      const newData = [...data];
+                      newData[index].availableSeats = e.target.value;
+                      setTrainData(newData);
+                    }}
+                  />
+                ) : (
+                  item.availableSeats
+                )}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                {item.editable ? (
+                  <input
+                    type="text"
+                    value={item.returnTimeFromArrivalStation}
+                    onChange={(e) => {
+                      const newData = [...data];
+                      newData[index].returnTimeFromArrivalStation =
+                        e.target.value;
+                      setTrainData(newData);
+                    }}
+                  />
+                ) : (
+                  item.returnTimeFromArrivalStation
+                )}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                {item.editable ? (
+                  <input
+                    type="text"
+                    value={item.arrivalTimeDepartureStation}
+                    onChange={(e) => {
+                      const newData = [...data];
+                      newData[index].arrivalTimeDepartureStation =
+                        e.target.value;
+                      setTrainData(newData);
+                    }}
+                  />
+                ) : (
+                  item.arrivalTimeDepartureStation
+                )}
+              </td>
               <td>
                 {item.editable ? (
                   <>
